4e9b19719f3a3147cc7f372ac81806e7a87acef6
Antonio Carlos Soares Prudencio <acsprudencio@gmail.com>
2023-01-06 11:37:15 -0300

    Desafios de codigo incompleto

--- src/main/java/org/desafios/QuadradosPerfeitos.java
+++ src/main/java/org/desafios/QuadradosPerfeitos.java
@@ -12,14 +12,28 @@ public class QuadradosPerfeitos {
         List<Integer> numeros = new ArrayList<>();
 
         //TODO: Com base no valor total, retorne o menor número de quadrados perfeitos.
-        int quadrados = 0;
+        int soma = 0;
         for (int i = 1; i<n; i++){
             double sqrt = Math.sqrt(i);
             if (Math.pow(Math.floor(Math.sqrt(i)),2) == i){
                 numeros.add(i);
             }
         }
+        int i = 1;
+        int contador = 0;
+        while (soma!=n){
+            int valor = numeros.get(numeros.size()-i);
+            if (soma < n){
+                soma += valor;
+                contador++;
+            }
+            if (soma > n){
+                soma -= valor;
+                i++;
+                contador--;
+            }
+        }
 
-        System.out.println(numeros);
+        System.out.println(contador);
     }
 }
